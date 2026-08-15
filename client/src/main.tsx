bfc526d84c83def68d6d6ccfb656ca8f7873e987
Ebuka Ben Umeh <ebuka.umeh40@outlook.com>
2026-08-15 23:18:20 +0100

    Initialise Customer 360 dialog bridge

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,9 +4,11 @@ import "./index.css";
 import "./lib/errorLogger";
 import { initPerformanceMonitor } from "./lib/performanceMonitor";
 import { initCustomer360AdminEnhancer } from "./components/admin/Customer360Enhancer";
+import { initCustomer360DialogBridge } from "./components/admin/customer360DialogBridge";
 
 initPerformanceMonitor();
 initCustomer360AdminEnhancer();
+initCustomer360DialogBridge();
 document.getElementById("customer-360-admin-enhancer-root")?.style.setProperty("pointer-events", "auto", "important");
 
 createRoot(document.getElementById("root")!).render(<App />);
