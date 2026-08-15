90c03cba70ee7f2a7b2b2fa892e87ebaa008c57c
Ebuka Ben Umeh <ebuka.umeh40@outlook.com>
2026-08-15 23:12:38 +0100

    Load Customer 360 admin enhancer

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,9 @@ import App from "./App";
 import "./index.css";
 import "./lib/errorLogger";
 import { initPerformanceMonitor } from "./lib/performanceMonitor";
+import { initCustomer360AdminEnhancer } from "./components/admin/Customer360Enhancer";
 
 initPerformanceMonitor();
+initCustomer360AdminEnhancer();
 
 createRoot(document.getElementById("root")!).render(<App />);
