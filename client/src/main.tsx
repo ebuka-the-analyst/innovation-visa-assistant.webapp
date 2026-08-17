f3da8fdbcc2bec10b776a3e40a3fd55ce36bc9c1
Ebuka Ben Umeh <ebuka.umeh40@outlook.com>
2026-08-17 18:17:35 +0100

    Load ADHD-friendly attention accessibility styles

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./attention-accessibility.css";
 import "./lib/errorLogger";
 import { initPerformanceMonitor } from "./lib/performanceMonitor";
 import { initQuestionnaireDraftSync } from "./lib/questionnaireDraftSync";
