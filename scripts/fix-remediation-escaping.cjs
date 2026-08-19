4df948af766b0e2bad01fc8d99ea9b942c3ccf94
Ebuka Ben Umeh <ebuka.umeh40@outlook.com>
2026-08-19 23:33:10 +0100

    Make questionnaire example remediation idempotent

--- scripts/fix-remediation-escaping.cjs
+++ scripts/fix-remediation-escaping.cjs
@@ -5,5 +5,9 @@ const twoBackslashes = '\\'.repeat(2);
 const oneBackslash = '\\';
 source = source.split(twoBackslashes + '`').join(oneBackslash + '`');
 source = source.split(twoBackslashes + '${').join(oneBackslash + '${');
+source = source.replace(
+  `  text = replaceOnce(\n    text,\n    '\"UK Visa Assistant - Benedict Umeh\"',\n    '\"VisaFlow - Immigration Guidance\"',\n    'replace named questionnaire example',\n  );`,
+  `  text = text.replaceAll(\"\\\"UK Visa Assistant - Benedict Umeh\\\"\", \"\\\"VisaFlow - Immigration Guidance\\\"\");`,
+);
 fs.writeFileSync(file, source);
-console.log('Normalised nested template escaping in remediation script');
+console.log('Normalised one-shot remediation script');
