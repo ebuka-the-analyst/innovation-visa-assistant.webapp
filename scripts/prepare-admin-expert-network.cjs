6b2aa5f1f0ad9f842bb2869bf7bd19d5df5f034c
Ebuka Ben Umeh <ebuka.umeh40@outlook.com>
2026-08-20 17:02:28 +0100

    Run expert onboarding preparation with builds

--- scripts/prepare-admin-expert-network.cjs
+++ scripts/prepare-admin-expert-network.cjs
@@ -96,3 +96,4 @@ update('client/src/pages/expert-booking.tsx', (source) => {
 });
 
 console.log('[admin-expert-network] admin network route preparation complete');
+require('./prepare-expert-onboarding.cjs');
